87b7c0ad8ef8d44ed0e8ffbc1c00efca49257b7f
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-07-02 22:25:31 +0300

    s

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+
 function App() {
-  return (
-      < div>
-        <h1>Start</h1>
-      </div>
-  );
+    return ( <
+        div >
+        <
+        h1 > Start < /h1> <
+        div > < /div>   <div></div >
+
+        <
+        /div>
+    );
 }
 
 export default App;
