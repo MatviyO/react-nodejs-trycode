a42acc1dde2b3d3d44e24efac772af9be73f64dd
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-07-03 15:52:18 +0300

    p

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Header } from "semantic-ui-react";
+import brace from 'brace';
+import AceEditor from "react-ace/types";
+
+import 'brace/mode/javascript'
 
 function App() {
-    return ( <
-        div >
-        <
-        h1 > Start < /h1> <
-        div > < /div>   <div></div >
-
-        <
-        /div>
+    return (
+        <div>
+            <div className="header">
+                <Header size='huge'>TryCode</Header>
+            </div>
+            <div className="editor">
+                <TextArea rows={30}  />
+            </div>
+        </div>
     );
 }
 
